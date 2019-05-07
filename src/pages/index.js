8cdc767c9813772e4c246e886d6680b438670a98
yeshoham <yeshoham@cisco.com>
2019-05-07 15:26:41 +0300

    Clean up home page links

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ const IndexPage = () => {
             <Head title="Home"/>
             <h1>Hello.</h1>
             <h2>I'm Yehuda, a full-stack developer living in Mishmar Hasharon</h2>
-            <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
         </Layout>
     )
 }
